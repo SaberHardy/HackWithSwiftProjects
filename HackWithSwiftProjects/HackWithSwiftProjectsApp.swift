22ebd0b46aa25b304926725bbd716d73f1714ada
SaberHardy <samirsamado5@gmail.com>
2022-07-08 16:26:06 +0300

    Initial Commit

---
+++ HackWithSwiftProjects/HackWithSwiftProjectsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HackWithSwiftProjectsApp.swift
+//  HackWithSwiftProjects
+//
+//  Created by MacBook on 08.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct HackWithSwiftProjectsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
